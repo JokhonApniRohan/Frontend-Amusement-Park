7625557fb66cfeb3a6eb10016dc80f36f9bd8fc9
Md, Tawhidur Rahman(Rohan) <tawhidr19@gmail.com>
2023-11-18 01:29:14 +0600

    Did something

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from "./App.js";
 import AppAdmin from "./Admin/App"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RidesPage from "./Rides/RidesPage.jsx";
-import Homepage from "./Homepage/Homepage.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,7 +13,6 @@ root.render(
               <Route path="/login" element={<App />} />
               <Route path="/admin" element={<AppAdmin />}/>
               <Route path="/rides" element={<RidesPage />} />
-              <Route path="/" element={<Homepage />} />
             </Routes>
         </React.StrictMode>
     </BrowserRouter>
