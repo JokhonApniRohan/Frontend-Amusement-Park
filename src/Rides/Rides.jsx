02f523ad0086ff5523c07c81693ca3758e076e42
Md, Tawhidur Rahman(Rohan) <tawhidr19@gmail.com>
2023-11-17 23:43:45 +0600

    added rides page

---
+++ src/Rides/Rides.jsx
@@ -0,0 +1,24 @@
+import "./Rides_style.css"
+
+import React from 'react';
+import './Rides_style.css';
+
+function Ridebox(props) {
+  const { imageSrc, name, description, price, availableTickets } = props;
+
+  return (
+    <div className="ride-box">
+      <div className="image-container">
+        <img src={imageSrc} alt={name} />
+      </div>
+      <div className="ride-details">
+        <p className="name">{name}</p>
+        <p className="description">{description}</p>
+        <p className="price">Price: {price}</p>
+        <p className="available-tickets">Available Tickets: {availableTickets}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Ridebox;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import AppAdmin from "./Admin/App"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RidesPage from "./Rides/RidesPage.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,6 +12,7 @@ root.render(
             <Routes>
               <Route path="/login" element={<App />} />
               <Route path="/admin" element={<AppAdmin />}/>
+              <Route path="/rides" element={<RidesPage />} />
             </Routes>
         </React.StrictMode>
     </BrowserRouter>
